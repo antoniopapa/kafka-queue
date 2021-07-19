e4158cd128b7f23c81a615c3613422100ee0ae3b
Antonio Papa <antoniopapa1991@gmail.com>
2021-07-19 17:00:20 +0200

    first commit

---
+++ src/KafkaServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kafka;
+
+use Illuminate\Support\ServiceProvider;
+
+class KafkaServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $manager = $this->app['queue'];
+
+        $manager->addConnector('kafka', function () {
+            return new KafkaConnector;
+        });
+    }
+}
